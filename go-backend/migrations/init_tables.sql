bced0c2943625bf5a09a881306a59e77a6622a18
Kur04ka <vanya.kurochkin.23@mail.ru>
2024-12-07 21:15:39 +0700

    make corrections

--- go-backend/migrations/init_tables.sql
+++ go-backend/migrations/init_tables.sql
@@ -8,3 +8,13 @@ CREATE TABLE IF NOT EXISTS refresh_sessions (
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    oauth_id VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    avatar VARCHAR(255),
+    PRIMARY KEY(id)
+)
